7dff4f20f33406e48e0f6b4a6e233a9a30865e38
Manwinder4u <mani.winder0007@gmail.com>
2025-03-16 17:07:25 +0530

    feat: added regax for delimeter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe StringCalculator do
   it "returns the sum of multiple numbers" do
     expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
   end
+
+  def extract_delimiter(input)
+    match = input.match(/^([;,:@#&$%^*(){}\[\]<>|\/\\?!~+=.-]+)/)
+    match ? match[1] : nil
+  end
+
+  it "extracts semicolon (;)" do
+    expect(extract_delimiter(";1;2")).to eq(";")
+  end
+
+  it "extracts comma (,)" do
+    expect(extract_delimiter(",3,4")).to eq(",")
+  end
+
+  it "extracts colon (:)" do
+    expect(extract_delimiter(":5:6")).to eq(":")
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,23 @@
 class StringCalculator
   def self.add(numbers)
-    return 0 if numbers.empty?
+    return 0 if numbers.nil? || numbers.strip.empty?
 
-    sanitized_numbers = numbers.gsub("\\n", "\n")
-    sanitized_numbers.split(/,|\n/).map(&:to_i).sum
+    numbers = numbers.to_s.strip
+    if numbers.start_with?("//")
+      delimiter_section = numbers.split("//")[1].split("\n")[0]
+      delimiter = StringCalculator.extract_special_delimiter(delimiter_section)
+      puts "Delimiter: #{delimiter}"
+    else
+      delimiter = ","
+    end
+
+    delimiter_regex = Regexp.escape(delimiter)
+    numbers_array = numbers.split(/[\n#{delimiter_regex}]/).map(&:to_i)
+    numbers_array.sum
+  end
+
+  def self.extract_special_delimiter(delimiter)
+    match = delimiter.match(/^([;,:@#&$%^*(){}\[\]<>|\/\\?!~+=.-]+)/)
+    match ? match[1] : nil
   end
 end
